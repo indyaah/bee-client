373173e8ee3cba767e966c7811aaad4a20107821
Rick Beton <rickb777@bitbucket.org>
2015-01-14 20:01:03 +0000

    Broken build - transition to using symlinks

--- rebuild.sh
+++ rebuild.sh
@@ -5,13 +5,13 @@ find . -type d -name target | xargs rm -rf
 
 ./mime-types.sh
 
-[ -f version.gradle ] || ./switch.sh
+[ -f gradle.properties ] || ./switch.sh
 
 hg log --verbose | ./hgChangeLog.sh > changelog.md
 
 for v in 2.9.0 2.9.1 2.9.2 2.9.3 2.10.1 2.11.1; do
   ./switch.sh $v
-  gradle clean build
+  ./gradlew clean build
 done
 
 sh -$- ./maven-metadata.sh
