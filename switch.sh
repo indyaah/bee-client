9d41f32c78250a7dfe55791694854a4e621faa9f
Rick Beton <rickb777@bitbucket.org>
2015-01-14 20:01:03 +0000

    Broken build - transition to using symlinks

--- switch.sh
+++ switch.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -e
 
-V=${1:-2.11.1}
+V=${1:-2.11.2}
 scalaVersion=$(echo $V |cut -d. -f-2)
 scalaMicroVersion=$(echo $V |cut -d. -f3-)
 
-echo "ext {" > version.gradle
-echo " scalaVersion = \"$scalaVersion\"" >> version.gradle
-echo " scalaMicroVersion = \"$scalaVersion.$scalaMicroVersion\"" >> version.gradle
-echo "}" >> version.gradle
+echo "scalaVersion = $scalaVersion" > gradle.properties
+echo "scalaMicroVersion = $scalaVersion.$scalaMicroVersion" >> gradle.properties
 
-cat version.gradle
+cat gradle.properties
