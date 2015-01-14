6931c6d478cb05ebfe0ee8f1dc74528ec0f29b97
Rick Beton <rickb777@bitbucket.org>
2015-01-14 20:42:27 +0000

    Gradle build is coming together.

--- rebuild.sh
+++ rebuild.sh
@@ -10,6 +10,7 @@ find . -type d -name target | xargs rm -rf
 hg log --verbose | ./hgChangeLog.sh > changelog.md
 
 for v in 2.9.0 2.9.1 2.9.2 2.9.3 2.10.1 2.11.1; do
+  [ -x /usr/bin/figlet ] && figlet Scala $v
   ./switch.sh $v
   ./gradlew clean build
 done
