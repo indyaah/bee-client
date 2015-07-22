ea29a4f6e96fefc563dd235debbc7b25f26b1f1d
Rick Beton <rickb777@bitbucket.org>
2015-07-22 17:05:18 +0100

    Preparation for releasing via Bintray.

--- switch.sh
+++ switch.sh
@@ -1,13 +1,10 @@
 #!/bin/sh -e
 
-V=${1:-2.11.2}
+V=${1:-2.11.7}
 scalaVersion=$(echo $V |cut -d. -f-2)
 scalaMicroVersion=$(echo $V |cut -d. -f3-)
 
 echo "scalaVersion = $scalaVersion" > gradle.properties
 echo "scalaMicroVersion = $scalaVersion.$scalaMicroVersion" >> gradle.properties
-
-rm -f dependencies.gradle
-ln -s dependencies-$scalaVersion.gradle dependencies.gradle
 
 cat gradle.properties
